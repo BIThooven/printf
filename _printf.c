23e95eaeff9e4f382c6ebe0c6f4a68867befee20
BIThooven <yassine.parkourist@gmail.com>
2023-08-20 15:12:52 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -1,135 +1,78 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
 /**
- * print_char - a function that prints a character
- * @types: type of arguments
- * @p_buff: an array of args
- * @flags: for active flags
- * @width: width
- * @precision: precision of specifier
- * @size: the size of the array
- * Return: returns 1
-*/
-int print_char(va_list types, char p_buff[], int flags, int width, int precision, int size)
+  * ffunc - a function that take the format for _printf function
+  * @format: format specifier
+  * Return: NULL or a pointer to the function;
+  */
+int (*ffunc(const char *format))(va_list)
 {
-	char c;
+	unsigned int i = 0;
+	cd_f fnd_f[] = {
+		{"c", p_char},
+		{"s", p_str},
+		{"i", p_int},
+		{"d", p_decimal},
+		{NULL, NULL}
+	};
 
-	c = va_arg(types, int);
-	p_buff[size] = c;
-	write(1, &c, 1);
-
-	return (1);
-}
-/**
- * print_string - a function that prints a string
- * @types: type of arguments
- * @p_buff: an array of args
- * @flags: for active flags
- * @width: width
- * @precision: precision of specifier
- * @size: the size of the array
- * Return: returns count
-*/
-int print_string(va_list types, char p_buff[], int flags, int width, int precision, int size)
-{
-	char *str;
-	int i, count;
-
-	str = va_arg(types, char *);
-	while (str && str[i])
+	while (fnd_f[i].s)
 	{
-		p_buff[size] = str[i];
-		write(1, &str[i], 1);
-		count++;
+		if (fnd_f[i].s[0] == (*format))
+			return (fnd_f[i].f);
 		i++;
 	}
-
-	return (count);
+	return (NULL);
 }
 /**
- * print_percent - a function that prints %
- * @types: type of arguments
- * @p_buff: an array of args
- * @flags: for active flags
- * @width: width
- * @precision: precision of specifier
- * @size: the size of the array
- * Return: returns 1
-*/
-int print_percent(va_list types, char p_buff[], int flags, int width, int precision, int size)
-{
-	(void)types;
-	(void)flags;
-	(void)width;
-	(void)precision;
-	(void)size;
-
-	p_buff[size] = '%';
-	write(1, "%", 1);
-
-	return (1);
-}
-/**
-*print_buffer - printing the content
-*@p_buff: an array of characters
-*@buff_ind: a pointer to the buffer to be printed
-*@size: size of the buffer
-*Return: void returns nothing
-*/
-void print_buffer(char p_buff[], int *buff_ind, int size)
-{
-	if (*buff_ind > 0)
-	{
-		write(1, p_buff, *buff_ind);
-		*buff_ind = 0;
-	}
-}
-/**
-*_printf - a function to print a buffer
-*@format: a format specifier
-*Return: returns count
-*/
+  * _printf - a function to give an output of a certain format
+  * @format: format specifier
+  * Return: the number of printed formats;
+  */
 int _printf(const char *format, ...)
 {
-	int i, count;
-	va_list list;
-	char p_buff[1024];
-	int buff_ind;
+	va_list args;
+	int (*f)(va_list);
+	unsigned int i, count;
 
 	i = 0;
 	count = 0;
-	buff_ind = 0;
-
 	if (format == NULL)
 	{
 		return (-1);
 	}
-
-	va_start(list, format);
-	while (format && format[i])
+	va_start(args, format);
+	while (format[i])
 	{
-		if (format[i] == '%' && format[i + 1])
+		while (format[i] != '%' && format[i])
 		{
+			_putchar(format[i]);
+			count++;
 			i++;
-			count += handle_print(format, &i, list, p_buff, 0, 0, 0, buff_ind);
-			print_buffer(p_buff, &buff_ind, sizeof(p_buff));
+		}
+		if (format[i] == '\0')
+		{
+			return (count);
+		}
+		f = ffunc(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(args);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+		{
+			return (-1);
+		}
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+		{
+			i += 2;
 		}
 		else
-		{
-			if (buff_ind >= sizeof(p_buff))
-			{
-				print_buffer(p_buff, &buff_ind, sizeof(p_buff));
-			}
-			p_buff[buff_ind++] = format[i];
-			count++;
-		}
-		i++;
+			i++;
 	}
-
-	print_buffer(p_buff, &buff_ind, sizeof(p_buff));
-	va_end(list);
-
+	va_end(args);
 	return (count);
 }
